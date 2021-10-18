b742acd0a2ce031103808821b3acaa15f1ca09cc
Mathew Harris <10959101@uvu.edu>
2021-10-18 14:34:46 -0600

    Added deltaTime
    Added Time.deltaTime to script and placed it at 22 m/s (approx. 50mph)

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
     {
         //Where we will code to move the vehicle forward. 
         //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward);
+        transform.Translate(Vector3.forward * Time.deltaTime * 22);
 
     }
 }
