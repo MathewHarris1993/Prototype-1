c1f106f950106de81c7763f7628247ee40b370a0
Mathew Harris <10959101@uvu.edu>
2021-10-18 20:01:37 -0600

    turnSpeed
    Created turnSpeed variable.

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 20.0f;
+    public float turnSpeed;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,7 @@ public class PlayerController : MonoBehaviour
         //Where we will code to move the vehicle forward. 
         //transform.Translate(0, 0, 1);
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
 
     }
 }
